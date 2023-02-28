8742cddc7a13f54320bd1a61f856148d7168ede0
anirbansarkar-tigeranalytics <anirban.sarkar@tigerananlytics.com>
2023-02-28 09:26:53 +0530

    Assignment 1.1 first question

---
+++ Assignment1.1/shell-lesson-data/exercise-data/proteins/redo-figure-3.sh
@@ -0,0 +1,5 @@
+  321  nano middle.sh 
+  322  nano sorted.sh
+  323  bash sorted.sh *.pdb ../creatures/*.dat
+  324  wc -l ethane.pdb 
+  325  history | tail -n 5 > redo-figure-3.sh
---
+++ Assignment1.1/shell-lesson-data/north-pacific-gyre/do-stats.sh
@@ -0,0 +1,6 @@
+# calculate stats for data files
+for datafile in "$@"
+do 
+	echo $datafile
+	bash goostats.sh $datafile stats-$datafile
+done
